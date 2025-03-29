44c4305353b26c5e501f32f16c5065dde8ccade2
Jatin Sharma <instinctultra13@gmail.com>
2025-03-29 08:36:33 +0530

    Filtering

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -4,7 +4,8 @@ import UserContext from './contexts/UserContext';
 import { useNavigate } from 'react-router-dom';
 
 function AllUsers() {
-  const [users, setUsers] = useState(null);
+  let [users, setUsers] = useState(null);
+  const [filteredUsers, setFilteredUsers] = useState(users);
   let [page, setPage] = useState({
     activePage: 1,
     totalPage: 1,
@@ -14,6 +15,8 @@ function AllUsers() {
   const [error, setErrors] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  // Setting Active Page COunt
   useEffect(() => {
     let fetchUsers = async () => {
       try {
@@ -23,6 +26,7 @@ function AllUsers() {
         if (!res.ok) throw data;
 
         setUsers(data.data);
+        setFilteredUsers(data.data);
         setPage((prevData) => ({
           ...prevData,
           totalPage: data.total_pages,
@@ -36,6 +40,10 @@ function AllUsers() {
     fetchUsers();
   }, [page.activePage]);
 
+  useEffect(() => {}, [users, filteredUsers]); // This will run when 'users' changes
+
+  // Checking if User is Logged In
+
   if (activeUser.user === null)
     return (
       <h2 className="mt-5 text-center text-2xl animate-pulse">Login First</h2>
@@ -44,7 +52,7 @@ function AllUsers() {
     return <h2>{error.users}</h2>;
   }
 
-  if (!users) {
+  if (!filteredUsers) {
     return (
       <>
         <h2 className="text-2xl font-semibold">Loading</h2>
@@ -61,10 +69,13 @@ function AllUsers() {
     arr.push(i);
   }
 
+  // Handling Edit
+
   function handleEdit(user) {
     navigate('users/edit/' + user.id);
   }
 
+  // Handling Delete
   async function handleDelete(u) {
     setLoading(true);
     try {
@@ -78,14 +89,68 @@ function AllUsers() {
       setLoading(false);
     }
   }
+
+  function handleFilter(e) {
+    if (e.target.value === 'first_name') {
+      let sortedUsers = [...users].sort((a, b) =>
+        a.first_name.localeCompare(b.first_name)
+      );
+
+      setFilteredUsers(sortedUsers);
+    }
+    if (e.target.value === 'last_name') {
+      let sortedUsers = [...users].sort((a, b) =>
+        a.last_name.localeCompare(b.last_name)
+      );
+      setFilteredUsers(sortedUsers);
+    }
+  }
+
+  function handleSearch(e) {
+    let searchVal = e.target.value.trim().toLowerCase();
+
+    let filterUsers = [...users].filter(
+      (u) =>
+        u.first_name.toLowerCase().includes(searchVal) ||
+        u.last_name.toLowerCase().includes(searchVal)
+    );
+
+    setFilteredUsers(filterUsers);
+  }
+
   return (
     <section className="p-5 m-auto lg:text-xl md:text-lg sm:text-base">
       <main>
         <h2 className="text-2xl font-semibold underline text-center">
           Listing all the users
         </h2>
+        <div className="flex justify-center items-center my-4">
+          <input
+            type="text"
+            name="search"
+            placeholder="Enter User Name"
+            className="border-[1px] border-solid p-1.5"
+            onChange={handleSearch}
+          />
+          <select
+            name="select"
+            className="p-1.5 ml-4 border-solid border-[1px]"
+            onChange={handleFilter}
+          >
+            <option value="filter" hidden>
+              Sort
+            </option>
+            <option value="first_name">First Name</option>
+            <option value="last_name">Last Name</option>
+          </select>
+        </div>
+        {filteredUsers.length === 0 && (
+          <h2 className="text-center text-2xl animate-bounce my-20">
+            No User Found
+          </h2>
+        )}
         <ul className="flex flex-wrap justify-center">
-          {users.map((u) => {
+          {filteredUsers.map((u) => {
             return (
               <li
                 key={u.id}
@@ -118,24 +183,26 @@ function AllUsers() {
           })}
         </ul>
       </main>
-      <footer className="text-center">
-        {arr.map((p) => {
-          return (
-            <button
-              key={p}
-              className="px-3 py-1 bg-neutral-400 m-1 text-white hover:bg-neutral-500 cursor-pointer a"
-              onClick={() => {
-                setPage((prevData) => ({
-                  ...prevData,
-                  activePage: p,
-                }));
-              }}
-            >
-              {p}
-            </button>
-          );
-        })}
-      </footer>
+      {filteredUsers.length !== 0 && (
+        <footer className="text-center">
+          {arr.map((p) => {
+            return (
+              <button
+                key={p}
+                className="px-3 py-1 bg-neutral-400 m-1 text-white hover:bg-neutral-500 cursor-pointer a"
+                onClick={() => {
+                  setPage((prevData) => ({
+                    ...prevData,
+                    activePage: p,
+                  }));
+                }}
+              >
+                {p}
+              </button>
+            );
+          })}
+        </footer>
+      )}
     </section>
   );
 }
